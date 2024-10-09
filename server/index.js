13dedbed3965133171851890015b118bfa659585
Kriiscor <Corentindubail@gmail.com>
2024-10-09 15:32:05 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const db = require("./models");
+app.use(express.json());
+app.use(cors());
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
+});

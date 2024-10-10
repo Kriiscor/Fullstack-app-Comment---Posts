01bb8be4afead06182a85007181c9404f836e738
Kriiscor <Corentindubail@gmail.com>
2024-10-10 19:10:30 +0200

    Ajout du login et de la registration coté serveur et front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
       <Router>
         <div className="navbar">
-          <Link to="/createpost">Create A Post</Link>
           <Link to="/">Go home page</Link>
+          <Link to="/createpost">Create A Post</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/registration">Registration</Link>
         </div>
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/createpost" Component={CreatePost} />
           <Route path="/post/:id" Component={Post} />
+          <Route path="/registration" Component={Registration} />
+          <Route path="/login" Component={Login} />
         </Routes>
       </Router>
     </div>
